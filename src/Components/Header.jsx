f71d281ca1195aff1c91ea7110f2c468653e00c3
Samue Almao <samuel.alamoinacapmail.cl>
2026-07-23 19:35:49 -0400

    guardado y arreglos en header y footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,14 @@
 function Header() {
   return (
     <>
-    <Header className="bg-black text-white text-center py-5 border-bottom border-secondary shadow-sm">
+    <header className="bg-black text-white text-center py-5 border-bottom border-secondary shadow-sm">
       <div className="container">
         <h1 className="display-4 fw-bold text-primary">Tienda Gamer</h1>
         <p className="lead text-muted mb-0">Descubre las mejores experiencia del mundo gamer.</p>
       </div>
-    </Header>
+    </header>
     </>
   );
 }
 
-export default Header
+export default Header;
