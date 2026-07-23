edd327fa833c2f1b8aadc755b12af55d64c1612b
Samue Almao <samuel.alamoinacapmail.cl>
2026-07-23 18:19:44 -0400

    testeo de fotter con bootstrap

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,10 @@
-function Header (){
-
-return(
-    <>
-    <header className="container-fluid text-center py-4">
-    <img src="" alt="" />
-
-    <h1>Tienda de Videojuegos</h1>
+function Header() {
+  return (
+    <header className="bg-black text-white text-center py-5 border-bottom border-secondary shadow-sm">
+      <div className="container">
+        <h1 className="display-4 fw-bold text-primary">Tienda Gamer</h1>
+        <p className="lead text-muted mb-0">Descubre las mejores experiencia del mundo gamer.</p>
+      </div>
     </header>
-    </>
-)
-
+  );
 }
-
-export default Header; 
