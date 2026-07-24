7cc4adaf0c914796adc3805f4ef24ce978f396c0
benjamin cuevas <benjamin.cuevas08@inacapmail.cl>
2026-07-23 21:37:07 -0400

    bannertamano

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <>
     <header className="bg-black text-white text-center py-5 border-bottom border-secondary shadow-sm">
       <div className="container">
-        <img src="/portadas/banner.png" alt="Imagen Tienda"  className="img-fluid mb-3 rounded" style={{ maxWidth: '150px' }}/>
+        <img src="/portadas/banner.png" alt="Imagen Tienda"  className="img-fluid mb-3 rounded" style={{ maxWidth: '450px' }}/>
         <h1 className="display-4 fw-bold text-primary">Tienda Gamer</h1>
         <p className="lead text-light mb-0">Descubre las mejores experiencia del mundo gamer.</p>
       </div>
